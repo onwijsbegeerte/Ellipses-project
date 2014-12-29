40345aa91ad5951f86d4af9ff294deebaafcf87a
onwijsbegeerte <mendel.agterberg@gmail.com>
2014-12-28 20:41:47 -0500

    Testing Around

--- js/sketch.js
+++ js/sketch.js
@@ -4,13 +4,45 @@ window.onload = function() {
   paper.setup('my-canvas');
 
   // Background
-  var background = new Path.Rectangle(view.bounds);
-      background.fillColor = '#000';
+    var background = new Path.Rectangle(view.bounds);
+        background.fillColor = '#000';
+    var circleArray = [];
+  	
 
-  paper.view.draw();
 
-  // Animation
-  paper.view.onFrame = function(event) {
+  	for (var i = 0; i < 50; i++) {
+	  	var x,y;
+	  	x = i * 50;
+	  	y = i * 50;
 
-  };
+	  	circleArray.push(new Path.Circle({
+		
+		  center: [x,y],
+		  radius: 30,
+		  strokeColor: 'red',
+		  fillColor: 'green'
+
+		}));
+
+  	};
+
+// Create a rectangle shaped path with its top left point at
+// {x: 75, y: 75} and a size of {width: 75, height: 75}
+var path = new Path.Rectangle({
+	point: [75, 75],
+	size: [75, 75],
+	strokeColor: 'black'
+});
+
+function onFrame(event) {
+	// Each frame, rotate the path by 3 degrees:
+	path.rotate(3);
+}
+
+    paper.view.draw();
+
+	// Animation
+	paper.view.onFrame = function(event) {
+
+	};
 };
